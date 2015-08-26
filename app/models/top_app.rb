df64ce17c38974aeb764c8091f41e90259add046
Alexandra Yamaui <alexandra.yamaui@gmail.com>
2015-08-26 15:01:26 -0430

    Finished the search by category, monetization and rank position

--- app/models/top_app.rb
+++ app/models/top_app.rb
@@ -7,9 +7,13 @@ class TopApp
   # @param category_id [String] The id of the apps category
   # @param monetization [String] The monetization type of the apps to fetch 
   #         (Paid, Free, Grossing)
-  # @return [Hash<String x Hash<String>>] meta_data The information extracted
+  # @param rank_position [Integer | nil] The rank position of the app to fetch,
+  #         if a single app is requested; nil, otherwise
+  # @return meta_data [Hash<String x Hash<String>>] The information extracted
   #         from the Apple Lookup API
-  def get_top_apps(category_id, monetization)
+  def get_top_apps(category_id, 
+                  monetization, 
+                  rank_position = 3)
     meta_data = {}
 
     # Fetches the top rated apps inside the category
@@ -44,32 +48,80 @@ class TopApp
       app_ids = response["topCharts"][2]["adamIds"]
     end
 
-    # Makes a request call to the search api for every 10 app ids
-    app_ids.in_groups_of(10) do |group|
-      ids = group.join(',')
-      response = HTTParty.get("https://itunes.apple.com/lookup", 
-                                query: {id: ids})
-      response = JSON.parse(response.body)
+    unless rank_position
+      fetch_meta_data_for_multiple_apps(app_ids, meta_data)
+    else
+      fetch_meta_data_for_single_app(app_ids[rank_position - 1], meta_data)
+    end
+      
 
-      # Extracts the meta data for every app
-      response["results"].each do |result|
+  end
 
-        meta_data[result["trackId"]] = {
-          app_name: result["trackName"],
-          description: result["description"],
-          small_icon_url: result["artworkUrl60"],
-          publisher_name: result["artistName"],
-          price: result["price"],
-          version_number: result["version"],
-          average_user_rating: result["averageUserRatingForCurrentVersion"]
-        } 
-      end
+  private
 
+    # Extracts the meta data required for a single app and stores it in the
+    # meta_data hash
+    #
+    # @param app_info [Hash<String x String>] App information from the look
+    #         up API
+    # @param meta_data [Hash<String x Hash<String>>] The hash where the
+    #         information is stored
+    # @return meta_data [Hash<String x Hash<String>>] The meta_data hash
+    #         updated with the app info
+    def extract_meta_data(app_info, meta_data)
+
+      meta_data[app_info["trackId"]] = {
+        app_name: app_info["trackName"],
+        description: app_info["description"],
+        small_icon_url: app_info["artworkUrl60"],
+        publisher_name: app_info["artistName"],
+        price: app_info["price"],
+        version_number: app_info["version"],
+        average_user_rating: app_info["averageUserRatingForCurrentVersion"]
+      }
 
     end
 
-    meta_data
+    # Fetches the meta data for multiple apps
+    #
+    # @param app_ids [Array[String]] An array with the apps ids
+    # @param meta_data [Hash<String x Hash<String>>] The hash where the
+    #         information is stored
+    # @return meta_data [Hash<String x Hash<String>>] The meta_data hash
+    #         updated with the apps info
+    def fetch_meta_data_for_multiple_apps(app_ids, meta_data)
 
-  end
+      # Makes a request call to the search api for every 10 app ids
+      app_ids.in_groups_of(10) do |group|
+        ids = group.join(',')
+        response = HTTParty.get("https://itunes.apple.com/lookup", 
+                                  query: {id: ids})
+        response = JSON.parse(response.body)
+
+        # Extracts the meta data for every app
+        response["results"].each do |app_info|
+
+          extract_meta_data(app_info, meta_data)
+        end
+      end
+
+      meta_data
+    end
+
+    # Fetches the meta data for a single app
+    #
+    # @param app_id [String] The app id
+    # @param meta_data [Hash<String x Hash<String>>] The hash where the
+    #         information is stored
+    # @return meta_data [Hash<String x Hash<String>>] The meta_data hash
+    #         updated with the app info
+    def fetch_meta_data_for_single_app(app_id, meta_data)
+
+      response = HTTParty.get("https://itunes.apple.com/lookup", 
+                              query: {id: app_id})
+      response = JSON.parse(response.body)
+
+      extract_meta_data(response["results"].first, meta_data)
+    end
 
 end
