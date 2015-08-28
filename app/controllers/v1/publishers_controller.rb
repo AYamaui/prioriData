b001939283961a84cde8bb518e7b73d42c2c4d06
Alexandra Yamaui <alexandra.yamaui@gmail.com>
2015-08-28 00:07:48 -0430

    Uncomented rescue block

--- app/controllers/v1/publishers_controller.rb
+++ app/controllers/v1/publishers_controller.rb
@@ -3,15 +3,15 @@ class V1::PublishersController < V1::BaseController
   before_action :check_parameters
 
   def index
-    # begin
+    begin
       @top_publishers = Publisher.get_top_publishers(@category_id, 
                                                       @monetization)
       render json: @top_publishers
 
-    # rescue
-    #   @errors = "Something went wrong!"
-    #   render template: 'v1/errors/errors.json.jbuilder'
-    # end
+    rescue
+      @errors = "Something went wrong!"
+      render template: 'v1/errors/errors.json.jbuilder'
+    end
   end
 
 end
